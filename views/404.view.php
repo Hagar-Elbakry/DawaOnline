487c8311506dfacf41450f6da9ee5b8f8ec03a68
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-02-01 21:18:37 +0200

    update 404 page

--- views/404.view.php
+++ views/404.view.php
@@ -13,7 +13,7 @@
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,700" rel="stylesheet">
 
 	<!-- Custom stlylesheet -->
-	<link type="text/css" rel="stylesheet" href="/css/stylee.css" />
+	<link type="text/css" rel="stylesheet" href="css/stylee.css" />
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -29,11 +29,11 @@
 	<div id="notfound">
 		<div class="notfound">
 			<div class="notfound-404">
-				<h1>4<span></span>4</h1>
+				<h1>4<span>0</span>4</h1>
 			</div>
 			<h2>Oops! Page Not Be Found</h2>
 			<p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
-			<a href="/">Back to homepage</a>
+			<a href="/Projects/DawaOnline/">Back to homepage</a>
 		</div>
 	</div>
 
