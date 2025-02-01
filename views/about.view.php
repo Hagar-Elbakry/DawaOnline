246b03ffe633f5aafe35e39763872d0d7199f36e
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-02-01 14:59:36 +0200

    refactor file structure

---
+++ views/about.view.php
@@ -0,0 +1,28 @@
+<?php require "partials/head.php"?>
+
+<body class="sub_page">
+  <div class="hero_area">
+
+    <!-- header section strats -->
+    <?php require "partials/header.php"?>
+    <!-- end header section -->
+
+  </div>
+
+  <!-- about section -->
+  <?php require "partials/about.php"?>
+  <!-- end about section -->
+
+  <!-- info section -->
+  <?php require "partials/info.php"?>
+  <!-- end info section -->
+
+  <!-- footer section -->
+  <?php require "partials/footer.php"?>
+  <!-- footer section -->
+   
+<?php require "partials/script.php"?>
+ 
+</body>
+
+</html>
---
+++ views/contact.view.php
@@ -0,0 +1,26 @@
+<?php require "partials/head.php"?>
+
+<body class="sub_page">
+  <div class="hero_area">
+
+    <!-- header section strats -->
+    <?php require "partials/header.php"?>
+    <!-- end header section -->
+  </div>
+
+  <!-- contact section -->
+  <?php require "partials/contact.php"?>
+  <!-- end contact section -->
+
+  <!-- info section -->
+  <?php require "partials/info.php"?>
+  <!-- end info section -->
+
+  <!-- footer section -->
+  <?php require "partials/footer.php"?>
+  <!-- footer section -->
+
+  <?php require "partials/script.php"?>
+</body>
+
+</html>
