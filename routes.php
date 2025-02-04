35d744eb83bebd757ed7cc2e3e114441d6bd2e3f
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-02-04 18:53:04 +0200

    implement middleware for authentication and guest access

--- routes.php
+++ routes.php
@@ -4,11 +4,11 @@
 $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
-$router->get('/product', 'controllers/product.php');
+$router->get('/product', 'controllers/product.php')->only('auth');
 
-$router->get('/cart', 'controllers/cart/show.php');
+$router->get('/cart', 'controllers/cart/show.php')->only('auth');
 $router->post('/cart', 'controllers/cart/store.php');
 $router->delete('/cart', 'controllers/cart/destroy.php');
 
-$router->get('/register', 'controllers/registration/create.php');
+$router->get('/register', 'controllers/registration/create.php')->only('guest');
 $router->post('/register', 'controllers/registration/store.php');
