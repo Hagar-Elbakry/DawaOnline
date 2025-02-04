410b9f8777b42d2613083b8ab13ba94da3458dc2
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-02-04 22:55:20 +0200

    implement authentication logic

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -1,36 +1,21 @@
 <?php
 
-use Core\Validator;
-use Core\App;
-use Core\Database;
 use Http\Forms\LoginForm;
-
-$db = App::resolve(Database::class);
+use Core\Authenticator;
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 $form = new LoginForm();
-if(!$form->validate($email, $password)) {
-    return view("session/create.view.php", [
-        'errors' => $form->errors()
-    ]);
-}
-
-$user = $db->query('SELECT * FROM users WHERE email = :email',[
-    ':email' => $email,
-])->fetch();
-
-if($user) {
-    if(password_verify($password, $user['password'])) {
-        login($user);
-        header('Location: /');
-        die();
+if($form->validate($email, $password)) {
+    if((new Authenticator)->attemp($email, $password)) {
+        redirect('/');
     }
+
+    $form->error('email', 'Invalid email or password.');
 }
+
 
 return view("session/create.view.php", [
-    'errors' => [
-        'email' => 'Invalid email or password.'
-    ]
+    'errors' => $form->errors()
 ]);
