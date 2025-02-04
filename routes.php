50918768b3e30eff0a278c27374b488d20a8787c
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-02-04 12:25:26 +0200

    enhance routing system

--- routes.php
+++ routes.php
@@ -1,9 +1,15 @@
 <?php
 
-return [
-    '/' => "controllers/index.php",
-    '/about' => "controllers/about.php",
-    '/contact' => "controllers/contact.php",
-    '/product' => "controllers/product.php",
-    '/cart' => "controllers/cart.php"
-];
+//return [
+//    '/' => "controllers/index.php",
+//    '/about' => "controllers/about.php",
+//    '/contact' => "controllers/contact.php",
+//    '/product' => "controllers/product.php",
+//    '/cart' => "controllers/cart.php"
+//];
+
+$router->get('/', 'controllers/index.php');
+$router->get('/about', 'controllers/about.php');
+$router->get('/contact', 'controllers/contact.php');
+$router->get('/product', 'controllers/product.php');
+$router->get('/cart', 'controllers/cart.php');
