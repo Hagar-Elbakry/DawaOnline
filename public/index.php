204b92a691eb6a088a0a284f75c6a6339399f548
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-02-05 13:46:17 +0200

    initialize project with Composer, add .gitignore and update autoloading

--- public/index.php
+++ public/index.php
@@ -5,12 +5,9 @@ use Core\Router;
 use Core\Session;
 use Core\ValidationException;
 const BASE_PATH = __DIR__ . "/../";
-require BASE_PATH . "core/functions.php";
 
-spl_autoload_register(function($class) {
-    $class = str_replace("\\", "/", $class);
-    require base_path("$class.php");
-});
+require BASE_PATH . "/vendor/autoload.php";
+require BASE_PATH . "core/functions.php";
 
 require base_path("bootstrap.php");
 
